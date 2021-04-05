820a62cdfed09e9bf74c9ef8d638c92a4c3bbf0e
hagiwara <roitoitoitoi@yahoo.co.jp>
2021-04-05 16:41:32 +0000

    バリデーション追加

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
+    
+    {{-- エラーメッセージ表示--}}
+    @if (count($errors) > 0)
+        <ul class="alert alert-danger" role="alert">
+            @foreach ($errors->get('content') as $error)
+                <li class="ml-4">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    
     <h1>メッセージ新規作成ページ</h1>
 
     <div class="row">
@@ -11,6 +20,13 @@
                 <div class="form-group">
                     {!! Form::label('content', 'メッセージ:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    
+                    @if ($errors->has('content'))
+                        @foreach($errors->get('content') as $error)
+                            <p style="color:red">{{ $error }}</p>
+                        @endforeach
+                    @endif
+                    
                 </div>
         
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
