3a1dc58f79a4dd42463383931f8882fa4a0ef95e
roiaki <roitoitoitoi@yahoo.co.jp>
2021-04-12 16:28:21 +0900

    test

--- resources/views/commons/error_content.blade.php
+++ resources/views/commons/error_content.blade.php
@@ -1,6 +1,9 @@
 @if ($errors->has('content'))
     @foreach($errors->get('content') as $error)
      
+
+
+    
     {{-- 上に0.50remのマージン --}}
     <div class="mt-2">
         <p class="alert alert-danger" role="alert">{{ $error }}</p>
